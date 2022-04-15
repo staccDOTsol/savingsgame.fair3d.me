bf755def0e4d625ffc508afb49bac6708c41bd1d
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-04-15 00:56:32 -0300

    pretty

--- components/fair-launch.ts
+++ components/fair-launch.ts
@@ -115,72 +115,10 @@ const getSetupForTicketing = async (
 
   let amountLamports = 0;
   //@ts-ignore
-  if (!fairLaunch.state.treasuryMint) {
     console.log(1)
    
       amountLamports = Math.ceil(amount * LAMPORTS_PER_SOL);
     
-  } else {
-    console.log(2)
-    const transferAuthority = anchor.web3.Keypair.generate();
-    signers.push(transferAuthority);
-    // NOTE this token impl will not work till you get decimal mantissa and multiply...
-    /// ex from cli wont work since you dont have a Signer, but an anchor.Wallet
-    /*
-    const token = new Token(
-        anchorProgram.provider.connection,
-        //@ts-ignore
-        fairLaunchObj.treasuryMint,
-        TOKEN_PROGRAM_ID,
-        walletKeyPair,
-      );
-      const mintInfo = await token.getMintInfo();
-      amountNumber = Math.ceil(amountNumber * 10 ** mintInfo.decimals);
-    */
-    instructions.push(
-      Token.createApproveInstruction(
-        TOKEN_PROGRAM_ID,
-        //@ts-ignore
-        fairLaunch.state.treasuryMint,
-        transferAuthority.publicKey,
-        anchorWallet.publicKey,
-        [],
-        //@ts-ignore
-
-        // TODO: get mint decimals
-        amountNumber + fairLaunch.state.data.fees.toNumber(),
-      ),
-    );
-
-    remainingAccounts.push({
-      //@ts-ignore
-      pubkey: fairLaunch.state.treasuryMint,
-      isWritable: true,
-      isSigner: false,
-    });
-    remainingAccounts.push({
-      pubkey: (
-        await getAtaForMint(
-          //@ts-ignore
-          fairLaunch.state.treasuryMint,
-          anchorWallet.publicKey,
-        )
-      )[0],
-      isWritable: true,
-      isSigner: false,
-    });
-    remainingAccounts.push({
-      pubkey: transferAuthority.publicKey,
-      isWritable: false,
-      isSigner: true,
-    });
-    remainingAccounts.push({
-      pubkey: TOKEN_PROGRAM_ID,
-      isWritable: false,
-      isSigner: false,
-    });
-  }
-
   return {
     remainingAccounts,
     instructions,
